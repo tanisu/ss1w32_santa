3a80b79caa7b2648cbd92d870789b28cef5a7a15
tacagi003 <92666044+Tatt1118@users.noreply.github.com>
2022-12-03 22:24:13 +0900

    ももさんUI使ってサンプル作ったよ

---
+++ Assets/takuma/Scripts/Panel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+    //矢印をクリックしたら画面遷移する　
+
+    public void OnCloickRigthArrow()
+    {
+        transform.localPosition = new Vector2(-1000,0);
+    }
+
+    public void OnCloickLiftYazirusi()
+    {
+
+    }
+
+
+}
